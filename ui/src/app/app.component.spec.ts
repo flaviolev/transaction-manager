b4cc3c6f9c2901dadcf63dddd70c54ce74627e1f
Flavio Böni <6606236+flaviolev@users.noreply.github.com>
2021-12-15 13:46:02 +0100

    add material ui

--- ui/src/app/app.component.spec.ts
+++ ui/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ui'`, () => {
+  it(`should have as title 'transaction-manager'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ui');
+    expect(app.title).toEqual('transaction-manager');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ui app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('transaction-manager app is running!');
   });
 });
