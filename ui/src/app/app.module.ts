71205f08c50d9fa43403901264188f5326392e13
Caldesi <luca.caldesi@zuehlke.com>
2021-12-16 10:10:33 +0100

    Replace references of User with Dashboard component

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { BoardUserComponent } from './board-user/board-user.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
@@ -12,6 +11,7 @@ import { RegisterComponent } from './register/register.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { authInterceptorProviders } from './core/helpers/auth.interceptor';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +20,7 @@ import { authInterceptorProviders } from './core/helpers/auth.interceptor';
     RegisterComponent,
     HomeComponent,
     ProfileComponent,
-    BoardUserComponent
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
