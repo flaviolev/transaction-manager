daacb50bf88d01db1dddc372793b88032c3f617c
Caldesi <luca.caldesi@zuehlke.com>
2021-12-16 13:22:44 +0100

    Put latest transactions and new payment component inside dashboard with simple card

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { authInterceptorProviders } from './core/helpers/auth.interceptor';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NewPaymentComponent } from './new-payment/new-payment.component';
 import { LatestTransactionsComponent } from './latest-transactions/latest-transactions.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { LatestTransactionsComponent } from './latest-transactions/latest-transa
     AppRoutingModule,
     NoopAnimationsModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MaterialModule
   ],
   providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
