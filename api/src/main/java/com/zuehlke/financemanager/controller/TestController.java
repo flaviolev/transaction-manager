0f180e627fa8f0b7ee3cc6cc6c74e0146e5dc763
Caldesi <luca.caldesi@zuehlke.com>
2021-12-17 07:53:51 +0100

    Remove unused user service in both api and ui

--- api/src/main/java/com/zuehlke/financemanager/controller/TestController.java
+++ api/src/main/java/com/zuehlke/financemanager/controller/TestController.java
@@ -1,6 +1,5 @@
 package com.zuehlke.financemanager.controller;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +14,5 @@ public class TestController {
         return "Public Content.";
     }
 
-    @GetMapping("/user")
-    @PreAuthorize("hasRole('USER')")
-    public String userAccess() {
-        return "User Content.";
-    }
 
 }
--- ui/src/app/core/services/user.service.ts
+++ ui/src/app/core/services/user.service.ts
@@ -1,20 +1,16 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs'
 
-const API_URL = 'http://localhost:8080/api/test/';
+const API_URL = 'http://localhost:8080/api/test/'
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getPublicContent(): Observable<any> {
-    return this.http.get(API_URL + 'all', { responseType: 'text' });
-  }
-
-  getUserBoard(): Observable<any> {
-    return this.http.get(API_URL + 'user', { responseType: 'text' });
+    return this.http.get(API_URL + 'all', { responseType: 'text' })
   }
 }
