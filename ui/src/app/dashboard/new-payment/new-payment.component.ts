a0abbfb6fce9d87b1b773d9ceb25898566c93de4
Caldesi <luca.caldesi@zuehlke.com>
2021-12-20 13:58:57 +0100

    Add custom validator to verify userExist against DB

--- ui/src/app/dashboard/new-payment/new-payment.component.ts
+++ ui/src/app/dashboard/new-payment/new-payment.component.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit } from '@angular/core'
 import { TokenStorageService } from '../../core/auth/token-storage.service'
-import { FormControl, FormGroup, Validators } from '@angular/forms'
+import {
+  AbstractControl,
+  AsyncValidatorFn,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms'
 import { UserService } from 'src/app/core/user.service'
+import { catchError, map, Observable } from 'rxjs'
 
 @Component({
   selector: 'app-new-payment',
@@ -24,11 +32,22 @@ export class NewPaymentComponent implements OnInit {
       from: new FormControl({ value: this.currentUser!, disabled: true }, [
         Validators.required,
       ]),
-      to: new FormControl(null, [Validators.required]),
+      to: new FormControl('', {
+        validators: [Validators.required],
+        asyncValidators: [this.userExistsValidator()],
+      }),
       amount: new FormControl(null, [Validators.required]),
     })
   }
 
+  userExistsValidator(): AsyncValidatorFn {
+    return (to: AbstractControl): Observable<ValidationErrors | null> => {
+      return this.userService.existsUser(to?.value).pipe(
+        map((exists) => (exists ? null : { userExist: false })),
+        catchError(async (err) => null),
+      )
+    }
+  }
   onHandlePayment() {
     const to = this.newPaymentForm.get('to')
     this.userService
