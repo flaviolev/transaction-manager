a3674f13943934155069cdb87f4e89ca9eecc148
Caldesi <luca.caldesi@zuehlke.com>
2021-12-20 09:51:22 +0100

    Access to current user in newpayment and  amount input as number

--- ui/src/app/dashboard/new-payment/new-payment.component.ts
+++ ui/src/app/dashboard/new-payment/new-payment.component.ts
@@ -1,18 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { TokenStorageService } from '../../core/auth/token-storage.service'
 
 @Component({
   selector: 'app-new-payment',
   templateUrl: './new-payment.component.html',
-  styleUrls: ['./new-payment.component.css']
+  styleUrls: ['./new-payment.component.css'],
 })
 export class NewPaymentComponent implements OnInit {
-
-  constructor() { }
+  currentUser: string | undefined
+  constructor(private tokenStorageService: TokenStorageService) {}
 
   ngOnInit(): void {
+    const user = this.tokenStorageService.getUser()
+    this.currentUser = user.username
   }
 
-  onHandlePayment() {
-    
-  }
+  onHandlePayment() {}
 }
