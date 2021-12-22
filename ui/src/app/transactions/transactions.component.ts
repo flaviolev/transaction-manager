e0e2c547dd6999496787c2c02b78f73183cb148c
Caldesi <luca.caldesi@zuehlke.com>
2021-12-22 13:05:34 +0100

    Bind template with component response and get rid of dummy data

--- ui/src/app/transactions/transactions.component.ts
+++ ui/src/app/transactions/transactions.component.ts
@@ -1,25 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Transaction } from '../core/transaction/transaction';
-import { TransactionService } from '../core/transaction/transaction.service';
-
-const TEST_DATA: Transaction[] = [
-  {source: "user1", target: 'user2', amount: 1079, balance: 179, creationDate: new Date()},
-  {source: "user2", target: 'user1', amount: 426, balance: 179, creationDate: new Date()},
-];
+import { Component, Input, OnInit } from '@angular/core'
+import { Transaction } from '../core/transaction/transaction'
+import { TransactionService } from '../core/transaction/transaction.service'
 
 @Component({
   selector: 'app-transactions',
   templateUrl: './transactions.component.html',
-  styleUrls: ['./transactions.component.css']
+  styleUrls: ['./transactions.component.css'],
 })
 export class TransactionsComponent implements OnInit {
+  transactions: Transaction[] = []
+  displayedColumns: string[] = [
+    'source',
+    'target',
+    'amount',
+    'balance',
+    'creationDate',
+  ]
 
-  transactions: Transaction[] = TEST_DATA;  
-  displayedColumns: string[] = ['source', 'target', 'amount', 'balance', 'creationDate'];
-  
-  constructor(private transactionService: TransactionService) { }
+  constructor(private transactionService: TransactionService) {}
 
   ngOnInit(): void {
-    this.transactionService.getTransactions().subscribe((res)=>this.transactions=res)
+    this.transactionService.getTransactions().subscribe((res) => {
+      console.log(res)
+      this.transactions = res
+    })
   }
 }
