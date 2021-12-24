9c50fce62750ded9d04c705c6ad8bb445bb4d51a
Caldesi <luca.caldesi@zuehlke.com>
2021-12-24 09:54:37 +0100

    Migrate login component to reactive forms

--- ui/src/app/login/login.component.ts
+++ ui/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { Subscription } from 'rxjs'
 import { AuthService } from '../core/auth/auth.service'
 import { TokenStorageService } from '../core/auth/token-storage.service'
@@ -10,19 +11,24 @@ import { UserStoreService } from '../core/user/userStore.service'
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  form: any = {
-    username: null,
-    password: null,
-  }
   isLoggedIn = false
   isLoginFailed = false
   errorMessage = ''
   subscription: Subscription | undefined
+  loginForm!: FormGroup
   constructor(
     private authService: AuthService,
     private tokenStorage: TokenStorageService,
     private userStore: UserStoreService,
-  ) {}
+  ) {
+    this.loginForm = new FormGroup({
+      username: new FormControl(null, [Validators.required]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    })
+  }
 
   ngOnInit(): void {
     this.subscription = this.userStore
@@ -34,9 +40,9 @@ export class LoginComponent implements OnInit, OnDestroy {
   }
 
   onSubmit(): void {
-    const { username, password } = this.form
-
-    this.authService.login(username, password).subscribe(
+    const username = this.loginForm.get('username')
+    const password = this.loginForm.get('password')
+    this.authService.login(username?.value, password?.value).subscribe(
       (data) => {
         this.tokenStorage.saveToken(data)
 
--- ui/src/app/shared/material.module.ts
+++ ui/src/app/shared/material.module.ts
@@ -1,13 +1,27 @@
 import { NgModule } from '@angular/core'
-import { MatCardModule} from '@angular/material/card'
+import { MatCardModule } from '@angular/material/card'
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input'
 import { MatButtonModule } from '@angular/material/button'
-import {MatTableModule} from '@angular/material/table'; 
+import { MatTableModule } from '@angular/material/table'
+import { MatToolbarModule } from '@angular/material/toolbar'
 
 @NgModule({
-  imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatTableModule],
-  exports: [MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatTableModule]
+  imports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule,
+    MatToolbarModule,
+  ],
+  exports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule,
+    MatToolbarModule,
+  ],
 })
 export class MaterialModule {}
- 
