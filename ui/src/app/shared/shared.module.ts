eaee446efe6a3ef649045c54547b5ce0b9555c4d
Caldesi <luca.caldesi@zuehlke.com>
2021-12-24 11:35:58 +0100

    Add filter for transacitons using pipe and input to show the filter

--- ui/src/app/shared/shared.module.ts
+++ ui/src/app/shared/shared.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core'
-import { ReactiveFormsModule } from '@angular/forms'
+import { ReactiveFormsModule, FormsModule } from '@angular/forms'
 import { BrowserModule } from '@angular/platform-browser'
+import { FilterTransactionsPipe } from './filter-transactions.pipe'
 import { MaterialModule } from './material.module'
 import { TransactionMatTableComponent } from './transaction-mat-table/transaction-mat-table.component'
 
 @NgModule({
-  declarations: [TransactionMatTableComponent],
-  imports: [MaterialModule, BrowserModule, ReactiveFormsModule],
-  exports: [MaterialModule, TransactionMatTableComponent, ReactiveFormsModule],
+  declarations: [TransactionMatTableComponent, FilterTransactionsPipe],
+  imports: [MaterialModule, BrowserModule, ReactiveFormsModule, FormsModule],
+  exports: [
+    MaterialModule,
+    TransactionMatTableComponent,
+    ReactiveFormsModule,
+    FilterTransactionsPipe,
+    FormsModule,
+  ],
 })
 export class SharedModule {}
