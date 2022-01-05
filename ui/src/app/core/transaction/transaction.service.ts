164e2fd291f350e164c698beab66ffed9cdaf737
Caldesi <luca.caldesi@zuehlke.com>
2022-01-05 15:04:36 +0100

    get rid of new transactions service

--- ui/src/app/core/transaction/transaction.service.ts
+++ ui/src/app/core/transaction/transaction.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Injectable } from '@angular/core'
-import { Observable } from 'rxjs'
+import { map, Observable } from 'rxjs'
 import { Transaction } from './transaction'
 
 const TX_API = 'http://localhost:8080/api/transaction/'
@@ -19,7 +19,21 @@ export class TransactionService {
     return this.http.post(TX_API, transaction, httpOptions)
   }
 
-  getTransactionsByUsername(): Observable<Transaction[]> {
-    return this.http.get<Transaction[]>(TX_API, httpOptions)
+  getTransactionsByUsername(fetchSize: number): Observable<Transaction[]> {
+    return this.http
+      .get<Transaction[]>(TX_API, httpOptions)
+      .pipe(
+        map((transactions: Transaction[]) =>
+          fetchSize
+            ? sortByDate(transactions).slice(0, fetchSize)
+            : sortByDate(transactions),
+        ),
+      )
   }
+}
+function sortByDate(transaction: Transaction[]): Transaction[] {
+  return transaction.sort(
+    (a: any, b: any) =>
+      new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
+  )
 }
