4d68d673a8e056a786925d6660607f8c78cc1afa
Caldesi <luca.caldesi@zuehlke.com>
2022-01-10 19:08:31 +0100

    Manage logout and snackbar

--- api/src/main/java/com/zuehlke/financemanager/payload/request/LoginRequest.java
+++ api/src/main/java/com/zuehlke/financemanager/payload/request/LoginRequest.java
@@ -2,12 +2,14 @@ package com.zuehlke.financemanager.payload.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
     @NotBlank
     private String username;
