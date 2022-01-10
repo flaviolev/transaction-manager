7f1dbe9e14a5180e0baafe17a5e969af963e19de
Caldesi <luca.caldesi@zuehlke.com>
2022-01-10 19:08:31 +0100

    Manage logout and snackbar

--- ui/src/app/core/auth/auth.interceptor.ts
+++ ui/src/app/core/auth/auth.interceptor.ts
@@ -10,6 +10,9 @@ import { BehaviorSubject, Observable, throwError } from 'rxjs'
 import { catchError, filter, switchMap, take } from 'rxjs/operators'
 import { TokenStorageService } from './token-storage.service'
 import { AuthService } from './auth.service'
+import { MatSnackBar } from '@angular/material/snack-bar'
+import { Router } from '@angular/router'
+import { UserStoreService } from '../user/userStore.service'
 const TOKEN_HEADER_KEY = 'Authorization' // for Spring Boot back-end
 
 @Injectable()
@@ -21,7 +24,10 @@ export class AuthInterceptor implements HttpInterceptor {
 
   constructor(
     private tokenService: TokenStorageService,
+    private userStore: UserStoreService,
     private authService: AuthService,
+    private _snackBar: MatSnackBar,
+    private router: Router,
   ) {}
 
   intercept(
@@ -36,7 +42,22 @@ export class AuthInterceptor implements HttpInterceptor {
 
     return next.handle(authReq).pipe(
       catchError((error) => {
-        console.log('err', error)
+        let errorMessage = ''
+        if (error.error instanceof ErrorEvent) {
+          // client-side error
+          errorMessage = `Error: ${error.error.message}`
+        } else {
+          // server-side error
+          errorMessage = error.error
+            ? error.error
+            : `Error Code: ${error.status}\nMessage: ${error.message}`
+        }
+        if (error.status !== 401) {
+          this._snackBar.open(errorMessage, 'Close', {
+            duration: 2000,
+            panelClass: ['snackbar-fail'],
+          })
+        }
 
         if (
           error instanceof HttpErrorResponse &&
@@ -45,10 +66,23 @@ export class AuthInterceptor implements HttpInterceptor {
         ) {
           return this.handle401Error(authReq, next)
         }
+        if (error.status === 403) {
+          this._snackBar.open('Session Expired. Please login again', 'Close', {
+            duration: 5000,
+            panelClass: ['snackbar-fail'],
+          })
+          this.signOut()
+        }
 
         return throwError(error)
       }),
     )
+  }
+
+  private signOut() {
+    this.tokenService.signOut()
+    this.userStore.setIsLoggedIn(false)
+    this.router.navigate(['/login'])
   }
 
   private handle401Error(request: HttpRequest<any>, next: HttpHandler) {
@@ -93,7 +127,6 @@ export class AuthInterceptor implements HttpInterceptor {
     })
   }
   private addTokenHeader(request: HttpRequest<any>, token: string) {
-    console.log('format normal', JSON.parse(token).token)
     return request.clone({
       headers: request.headers.set(
         TOKEN_HEADER_KEY,
@@ -104,5 +137,10 @@ export class AuthInterceptor implements HttpInterceptor {
 }
 
 export const authInterceptorProviders = [
-  { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true,
+    // deps: [MatSnackBar],
+  },
 ]
